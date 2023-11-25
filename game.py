1a8118d6aed0d7a349e0a46ed5e853f9cb90459a
Jagerlion <77819876+Jagerlion@users.noreply.github.com>
2023-11-25 14:55:56 -0600

    First push onto GitHub.

--- game.py
+++ game.py
@@ -38,4 +38,27 @@ def roll_off(handicap1,handicap2):
     print('Player 1 got '+str(sum1)+'; Player 2 got '+str(sum2)+'.')
     return sum1 > sum2
 
-# making a test currently
+def rollem(first,last):
+    """
+    Returns the sum of two random numbers.
+
+    The numbers generated are between first and last (inclusive).
+
+    Example: rollem(1,6) can return any value between 2 and 12.
+
+
+    Parameter first: The lowest possible number
+    Precondition: first is an integer
+
+    Parameter last: The greatest possible number
+    Precondition: last is an integer, last >= first
+    """
+    #print('Choosing two numbers between '+str(first)+' and '+str(last)+'.')
+
+    # Generate two numbers, add, and print the result
+    num1 = random.randint(first,last)
+    num2 = random.randint(first,last)
+    thesum = num1+num2
+    #print('The sum is '+str(thesum)+'.')
+
+    return thesum
