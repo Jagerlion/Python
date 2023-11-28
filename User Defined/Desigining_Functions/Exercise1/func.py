104ecd23526d6424f9d4bf2a3e8957435a875776
Jagerlion <77819876+Jagerlion@users.noreply.github.com>
2023-11-27 21:05:42 -0600

    Correction: this is new func.py for exercise1 of the design module

---
+++ User Defined/Desigining_Functions/Exercise1/func.py
@@ -0,0 +1,13 @@
+"""
+An empty module to illustrate function stubs.
+
+This module will contain function stubs, which are callable but do not do anything.
+
+Author: Bryan Ruiz
+Date: 11/27/2023
+"""
+import introcs
+
+
+def initials(n):
+    # Return the initials for n
