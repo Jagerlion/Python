d5a8f2c115d7b446a8c87dd78b394cd6a0092f5c
Jagerlion <77819876+Jagerlion@users.noreply.github.com>
2023-11-27 23:24:07 -0600

    Designing_Functions files updated

--- User Defined/Desigining_Functions/Exercise1/func.py
+++ User Defined/Desigining_Functions/Exercise1/func.py
@@ -8,6 +8,21 @@ Date: 11/27/2023
 """
 import introcs
 
-
 def initials(n):
     # Return the initials for n
+
+    """
+    Returns: The initials <first>. <last>. of the given name.
+
+    We assume that n is just two names (first and last). Middle names are
+    not supported.
+
+    Example: initials('John Smith') returns 'J. S.'
+    Example: initials('Walker White') returns 'W. W.'
+
+    Parameter n: the person's name
+    Precondition: n is in the form 'first-name last-name' with one space
+    between names. There are no spaces in either <first-name> or <last-name>
+    """
+
+    pass
