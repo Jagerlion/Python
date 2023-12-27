2109b11df72d790ac9addf96e97194693f2ce5a1
Jagerlion <77819876+Jagerlion@users.noreply.github.com>
2023-12-27 09:44:21 -0600

    first IF statement

---
+++ ecornell/controlling_flow/basic_if.py
@@ -0,0 +1,16 @@
+# a basic if statement
+
+
+# if statement:  indent after this line
+    # statement 1
+    # statement 2
+
+
+# example
+
+x = 2
+
+if x > 0:
+    print('Hello Javier')
+
+print('Goodbye Javier')
