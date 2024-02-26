0cfe308756b88a49592bd88d4acec1bd32aa738c
Jagerlion <77819876+Jagerlion@users.noreply.github.com>
2024-02-26 17:03:39 -0600

    Moved example file, created example2, created exericse 4 fun & test file

---
+++ ecornell/controlling_flow/conditional_statements/exercise4/example2.py
@@ -0,0 +1,14 @@
+"""
+Testing conditional statements and practice, this is example2 showing a difference
+between this and 1.
+
+Author: Bryan Ruiz
+Date: 02/25/2024
+"""
+
+x = 1
+y = 2
+
+z = x if x > y else y # this is the conditional statement, essentially if-else written into value
+
+print(z)
