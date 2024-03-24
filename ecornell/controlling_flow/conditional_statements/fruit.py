3f554e3b602c0915948a6f934e49e62c6255cfd0
Jagerlion <77819876+Jagerlion@users.noreply.github.com>
2024-03-24 15:15:16 -0500

    Basic double return conditional

---
+++ ecornell/controlling_flow/conditional_statements/fruit.py
@@ -0,0 +1,16 @@
+"""
+A module with a fruitful function definnition
+
+Author: Walker M . White
+Date: Feburary 14, 2019
+"""
+
+
+def plus(n):
+    """
+    Returns n+1
+    """
+
+    x = n + 1
+
+    return x
